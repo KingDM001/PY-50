2ae15a3458a94a937577f9d58f34ec47d1b536e4
king <kingdavidm77@gmail.com>
2025-07-26 16:22:32 +0100

    First Commit

---
+++ RES/myfirstjs.js
@@ -0,0 +1,23 @@
+document.title= "time"
+
+let name = window.prompt("what is your name");
+console.log(`hello, ${name}`);
+console.warn("name")
+console.error("page close")
+console.clear()
+/*
+hello
+this
+is
+a
+multi
+line
+comment*/
+name="mary"
+Console.log (name)
+let home =window.prompt ("what is it")
+console.log(home)
+//windows.close()
+
+const h1=document.getElementById('good')
+h1.innerText = 'hello' (name)
